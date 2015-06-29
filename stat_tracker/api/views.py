b2dd691edf0b8d2d0447548cdb57747ef9a2a140
Keith Calise <KeithCalise@gmail.com>
2015-06-29 18:06:50 -0400

    error-donottouch

--- stat_tracker/api/views.py
+++ stat_tracker/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+from api.Serializers import ActivitySerializer, StatsSerializer
+from api.models import Activity, Stats
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+
+
+class StatsViewSet(viewsets.ModelViewSet):
+    queryset = Stats.objects.all()
+    serializer_class = StatsSerializer
+
--- stat_tracker/stat_tracker/urls.py
+++ stat_tracker/stat_tracker/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from rest_framework import routers
+from django.views.generic import TemplateView, CreateView
+from rest_framework.routers import DefaultRouter
+from api import views
+
+
+router = routers.DefaultRouter()
+router.register(r'activities', views.ActivityViewSet)
+router.register(r'stats', views.StatsViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^api/', include(router.urls)),
+
 ]
