fe57f601f32621617e20e290403f606e88300f0e
Manish Patel <maddypatel@gmail.com>
2015-06-29 15:17:07 -0400

    Added models

--- stat_tracker/api/models.py
+++ stat_tracker/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Activity(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    posted_at = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Stats(models.Model):
+    user = models.ForeignKey(User)
+    activity = models.ForeignKey(Activity)
+    value = models.IntegerField()
+    posted_at = models.DateField()
+
+    def __str__(self):
+        return "{} --> {} --> {} --> {}".format(self.user, self.activity, self.value, self.posted_at)
