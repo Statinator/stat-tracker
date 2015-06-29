08eda932c1683543f3d69111ca46a85f525d0305
Manish Patel <maddypatel@gmail.com>
2015-06-29 17:20:23 -0400

    Added basic views and serializers

--- stat_tracker/api/views.py
+++ stat_tracker/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .models import Activity, Stats
+from .serializer import ActivitySerializer, StatsSerializer
 
-# Create your views here.
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+
+class StatsViewSet(viewsets.ModelViewSet):
+    queryset = Stats.objects.all()
+    serializer_class = StatsSerializer
