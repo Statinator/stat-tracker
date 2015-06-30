fe634ffc27701f8d9f778352735b680b93742712
Manish Patel <maddypatel@gmail.com>
2015-06-29 23:42:35 -0400

    added login

--- stat_tracker/stat_tracker/urls.py
+++ stat_tracker/stat_tracker/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^api/', include(router.urls)),
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
